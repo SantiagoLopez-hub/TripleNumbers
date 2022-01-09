f6ce01ee47e544b3a62458e12d682cd3e030b263
Santiago <silp4477@gmail.com>
2022-01-09 00:31:04 +0000

    Implementing logic of the game

--- main.cpp
+++ main.cpp
@@ -8,7 +8,33 @@ void PrintIntroduction(int Difficulty) {
     cout << " secure server room\nEnter the correct pattern to continue...\n\n";
 }
 
-bool PlayGame(int Difficulty) {}
+bool PlayGame(int Difficulty) {
+    PrintIntroduction(Difficulty);
+    const int a = rand() % Difficulty + Difficulty;
+    const int b = rand() % Difficulty + Difficulty;
+    const int c = rand() % Difficulty + Difficulty;
+    const int sum = a + b + c;
+    const int product = a * b * c;
+    int GuessA, GuessB, GuessC;
+
+    cout << "\tThere are 3 numbers in the code.\n";
+    cout << "\tThe sum of these 3 numbers will output: " << sum;
+    cout << "\n\tThe multiplication of the same numbers will output: " << product;
+
+    cout << "\n\nEnter your guess: ";
+    cin >> GuessA >> GuessB >> GuessC;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if ((GuessSum == sum) && (GuessProduct == product)) {
+        cout << "\nWell done!\n";
+        return true;
+    }
+    else {
+        cout << "\nIncorrect... Try again.\n";
+        return false;
+    }
+}
 
 void FinishMessage() {}
 
